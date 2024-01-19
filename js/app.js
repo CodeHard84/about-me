a91e0e1a91cfe86c0a9d5a427bd905dda7ff5dff
Jace <sockgnome@att.net>
2024-01-18 19:19:09 -0600

    No more comments

--- js/app.js
+++ js/app.js
@@ -1,16 +1,8 @@
 'use strict';
 
-// Track the correct answers
 let score = 0;
 
-// Modeled the array after: https://stackoverflow.com/questions/40601896/how-to-get-the-value-from-an-array-which-has-key-value-pair-objects
 const bioQuestions = [
-  // quetion = question
-  // answer = answer
-  // congratulate = congratulate
-  // giveAnswer = give answer
-  // questionType = type YN, INT, TF, etc..
-  // allowedAttempts = How many tries?
   {
     question: 'Is my favorite color orange?',
     answer: 'yes',
@@ -48,7 +40,7 @@ const bioQuestions = [
   },
   {
     question: 'I am thinking of a number between 1-10, what is it?',
-    answer: Math.floor(Math.random() * (10 - 1 + 1) + 1), // Added random during code review/pair programming lab04.
+    answer: Math.floor(Math.random() * (10 - 1 + 1) + 1),
     questionType: 'INT',
     allowedAttempts: 4
   },
@@ -66,33 +58,25 @@ function askGenericQuestions(questions) {
   for (const q of questions) {
     let answer;
     switch (q.questionType) {
-    case 'YN': // Yes or no questions.
+    case 'YN':
       answer = sanitizeInput(promptUser(q.question), 'str');
-      // Now we popped the question, let's check the input.
       while (answer === '' || answer === null || answer !== 'yes' && answer !== 'no'){
         answer = sanitizeInput(promptUser(q.question + ' Valid input: yes/no or y/n'), 'str');
       }
-      // Check answer
       if (answer === q.answer) {
         score++;
-        messageUser(q.congratulate); // They got it!
+        messageUser(q.congratulate);
       } else {
-        messageUser(q.giveAnswer); // They didn't get it.
-        // We could ask again here, but no need for now.
+        messageUser(q.giveAnswer);
       }
       break;
-    case 'TF': // True or false questions.
+    case 'TF':
       answer = promptUser(q.question);
       break;
-    case 'INT': { // Questions expecting an integer back.
-      // Think I remember something about prompt only returning a string.... and I was right.
-      // parseInt: https://www.sololearn.com/en/Discuss/2952070/javascript-user-input-solved
-      // isInteger: https://www.tutorialspoint.com/How-to-check-if-a-variable-is-an-integer-in-JavaScript
+    case 'INT': {
       let tracker = 0;
-      // Learned a new rule here: https://eslint.org/docs/latest/rules/no-case-declarations
-      while(tracker <= q.allowedAttempts - 1) { // Account for starting at 0. Better way for this?
+      while(tracker <= q.allowedAttempts - 1) {
         answer = parseInt(promptUser(q.question));
-        // Check the answer
         if(answer === q.answer) {
           score++;
           messageUser('You got the number in ' +(tracker + 1)+ ' tries!');
@@ -107,7 +91,6 @@ function askGenericQuestions(questions) {
           if (answer < q.answer){
             messageUser('My number is greater than ' +answer+ '.');
           } else if (!Number.isInteger(answer) || answer > 10){
-            // We could credit the user another try here by using tracker--, but that's not part of lab03.
             messageUser('Try to use a number between 1-10');
           }
         }
@@ -115,35 +98,32 @@ function askGenericQuestions(questions) {
       }
       break;
     }
-    // Turns out the case was a great idea =)
     case 'MC': {
       let gotIt = false;
-      let tries = q.allowedAttempts; // 6
+      let tries = q.allowedAttempts;
       while (tries > 0 && gotIt !== true){
-        // Pop the question
         answer = promptUser(q.question);
         if (answer === null){
           answer = '';
         }
         answer = answer.toLowerCase();
-        // Iterate through the array and compare.
         for (const correctAnswer of q.answer){
           if (answer === correctAnswer.toLowerCase()){
             score++;
             messageUser(q.congratulate);
-            gotIt = true; // I feel like this is hacky but I couldn't get out of this dang nested while loop.
+            gotIt = true;
           }
         }
         tries--;
       }
       let listAnswers = '';
       if (tries === 0){
-        for (const answers of q.answer) { listAnswers += answers + ' '; } // Made this a one liner because it looks better.
+        for (const answers of q.answer) { listAnswers += answers + ' '; }
         messageUser('Sorry the correct answers were: ' + listAnswers + ' better luck next time!');
       }
       break;
     }
-    default: // Should never get here. EVER.
+    default:
       break;
     }
   }
@@ -156,13 +136,10 @@ function promptUser(question){
 
 function sanitizeInput(inputValue, inputType) {
   if (inputType === 'str'){
-    // Make sure we are not calling toLowerCase on a null type.
     if (inputValue === null){
       inputValue = '';
     }
-    // Ok answer can't be null so let's go ahead and call toLowerCase.
     inputValue = inputValue.toLowerCase();
-    // Sanitize the answer a bit.
     if (inputValue === 'y'){
       inputValue = 'yes';
     } else if (inputValue === 'n') {
@@ -172,25 +149,14 @@ function sanitizeInput(inputValue, inputType) {
   }
 }
 
-// Ok, now I am going to create a function called kickoff which will start all my other
-// functions.
 function kickOff() {
-  // Let's get the userName and greet them!
   let userName = getUserName();
   messageUser('Hey ' + userName + ' welcome to my site!');
 
-  // Let's ask the user questions...
-  // Passing the function an array as an argument.
-  // Decided to do it this way in case we want more than
-  // one array of questions. Going to let user completely
-  // bypass this if username is 'cc';
   if (userName !== 'cc'){
     askGenericQuestions(bioQuestions);
   }
-  // Any other functions can be added here to make sure
-  // they are executed when the page is loading.
 
-  // Final message
   if (userName !== 'cc'){
     messageUser('Thanks for playing ' + userName + ' you scored ' + score + '/' + bioQuestions.length + '!');
   } else {
@@ -198,18 +164,13 @@ function kickOff() {
   }
 }
 
-// Will get invoked in kickoff
 function getUserName() {
   let userName = prompt('What is your name?');
   return userName;
 }
 
-// Need to invoke outside of kickoff
 function messageUser(msg) {
-  // This might seem redundant now, but I am sure we will stop using alerts
-  // and switch to some other form of messaging. When we do, I'll be ready!
   alert(msg);
 }
 
-// Let's kick this party off
 kickOff();
